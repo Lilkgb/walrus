781b7ad59712949720e768b069677b895b7a7754
Randee Layosa and Ryan McLean <randee@>
2019-01-15 10:53:06 -0800

    Add font color to background buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,25 @@ $(document).ready(function() {
       $(".whale-hidden").hide();
     });
   });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  });
+
 });
